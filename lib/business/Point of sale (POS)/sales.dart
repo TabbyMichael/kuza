f48db6cecd1e0a75e126654fdbf61e6ae898a209
TabbyMichael <120185762+TabbyMichael@users.noreply.github.com>
2024-04-06 10:10:18 +0300

    Sales & My cart

--- lib/business/Point of sale (POS)/sales.dart
+++ lib/business/Point of sale (POS)/sales.dart
@@ -1,44 +1,120 @@
 import 'package:flutter/material.dart';
+import 'package:kuza/business/Point%20of%20sale%20(POS)/cart/cart.dart';
+import 'package:kuza/business/Point%20of%20sale%20(POS)/cart/grocery_tile.dart';
+import 'package:kuza/business/Point%20of%20sale%20(POS)/cart_model.dart';
 import 'package:kuza/pages/home_page.dart';
+import 'package:provider/provider.dart';
 
 class Sales extends StatelessWidget {
-  const Sales({super.key});
+  const Sales({Key? key});
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'Sales',
-            style: TextStyle(
-              fontSize: 25,
-              fontWeight: FontWeight.w500,
-              color: Colors.black,
+  Widget build(BuildContext context) {
+    final cartModel =
+        Provider.of<CartModel>(context); // Create an instance of CartModel
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Sales',
+          style: TextStyle(
+            fontSize: 25,
+            fontWeight: FontWeight.w500,
+            color: Colors.black,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.lightBlueAccent,
+        leading: GestureDetector(
+          onTap: () {
+            Navigator.of(context).pushReplacement(MaterialPageRoute(
+              builder: (context) =>
+                  const HomePage(), // Navigate to the HomePage
+            ));
+          },
+          child: const Icon(
+            Icons.arrow_back,
+            color: Colors.black,
+          ), // Use the back arrow icon
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.black,
+        onPressed: () => Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return CartPage();
+            },
+          ),
+        ),
+        child: const Icon(Icons.shopping_bag),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(height: 48),
+          // good morning bro
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 24.0),
+            child: Text('Good morning,'),
+          ),
+          const SizedBox(height: 4),
+          // Let's order fresh items for you
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24.0),
+            child: Text(
+              "Let's order fresh items for you",
+              style: TextStyle(
+                fontSize: 36,
+                fontWeight: FontWeight.bold,
+              ),
             ),
           ),
-          centerTitle: true,
-          backgroundColor: Colors.lightBlueAccent,
-          leading: GestureDetector(
-            onTap: () {
-              Navigator.of(context).pushReplacement(MaterialPageRoute(
-                builder: (context) => const HomePage(), // Navigate to the HomePage
-              ));
-            },
-            child: const Icon(
-              Icons.arrow_back,
-              color: Colors.black,
-            ), // Use the back arrow icon
+          const SizedBox(height: 24),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 24.0),
+            child: Divider(),
           ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            // Add your action here when the button is clicked
-          },
-          backgroundColor: Colors.lightBlueAccent,
-          child: const Icon(
-            Icons.add,
-            size: 36,
-            color: Colors.white,
+          const SizedBox(height: 24),
+          // categories -> horizontal listview
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24.0),
+            child: Text(
+              "Fresh Items",
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            ),
           ),
-        ),
-      );
+          // recent orders -> show last 3
+          Expanded(
+            child: Consumer<CartModel>(
+              builder: (context, value, child) {
+                return GridView.builder(
+                  padding: const EdgeInsets.all(12),
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemCount: value.shopItems.length,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    childAspectRatio: 1 / 1.2,
+                  ),
+                  itemBuilder: (context, index) {
+                    return GroceryItemTile(
+                      itemName: value.shopItems[index][0],
+                      itemPrice: value.shopItems[index][1],
+                      imagePath: value.shopItems[index][2],
+                      color: value.shopItems[index][3],
+                      onPressed: () =>
+                          cartModel.addItemToCart(index), // Add item to cart
+                    );
+                  },
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
