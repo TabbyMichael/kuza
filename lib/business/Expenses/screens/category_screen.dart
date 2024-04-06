a3dc2be63f17b397f2afa39febc50fc01c3d3f9b
TabbyMichael <120185762+TabbyMichael@users.noreply.github.com>
2024-04-06 15:36:53 +0300

    Expense Section : Appbar and a name change

--- lib/business/Expenses/screens/category_screen.dart
+++ lib/business/Expenses/screens/category_screen.dart
@@ -3,13 +3,16 @@ import '../widgets/category_screen/category_fetcher.dart';
 import '../widgets/expense_form.dart';
 
 class CategoryScreen extends StatelessWidget {
-  const CategoryScreen({super.key});
+  const CategoryScreen({Key? key}) : super(key: key);
+
   static const name = '/category_screen'; // for routes
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Categories'),
+        title: const Text('Expenses'),
+        backgroundColor: Colors.lightBlueAccent, // Set the background color
       ),
       body: const CategoryFetcher(),
       floatingActionButton: FloatingActionButton(
